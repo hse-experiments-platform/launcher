c47cf152e7acf5da370d1273d5011962d96acc3c
ltulyavko <wot.k@mail.ru>
2024-03-21 17:19:20 +0300

    wip

--- internal/app/launcher/service.go
+++ internal/app/launcher/service.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hse-experiments-platform/launcher/internal/pkg/storage/db"
 	pb "github.com/hse-experiments-platform/launcher/pkg/launcher"
 	"github.com/hse-experiments-platform/library/pkg/utils/token"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -16,13 +16,13 @@ var _ pb.LauncherServiceServer = (*launcherService)(nil)
 
 type launcherService struct {
 	pb.UnimplementedLauncherServiceServer
-	commonDBConn *pgx.Conn
+	commonDBConn *pgxpool.Pool
 	maker        token.Maker
 
 	commonDB *db.Queries
 }
 
-func NewService(commonDBConn *pgx.Conn, maker token.Maker) *launcherService {
+func NewService(commonDBConn *pgxpool.Pool, maker token.Maker) *launcherService {
 	return &launcherService{
 		commonDBConn: commonDBConn,
 		maker:        maker,
